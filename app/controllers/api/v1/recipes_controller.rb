c609f094da2a2d153d643a6ae50c464de8c97506
Jamie Birney <jtbirney@gmail.com>
2017-11-02 15:56:50 -0400

    Switch to API instead of test data

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -22,72 +22,72 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     # CODE TO GET RECIPES FROM EDEMAN API
-    # response = HTTParty.get(url)
-    # hits = response.parsed_response["hits"]
-    # recipes = []
-    #
-    # if hits.count > 0
-    #   hits.each do |hit|
-    #     url = hit["recipe"]["url"]
-    #     title = hit["recipe"]["label"]
-    #     image = hit["recipe"]["image"]
-    #     recipes.push({ url: url, title: title, image: image })
-    #   end
-    # end
-
-    # Test data to avoid making repeated API calls
-    response = {
-      "hits": [
-        {
-          "recipe": {
-            "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_121b40b4af5f8f9c3dbc0272cc1a70a5",
-            "label": params[:query],
-            "image": "https://www.edamam.com/web-img/b4e/b4ecf425e5fcd21390a1203976f5fef8.jpg",
-            "source": "Epicurious",
-            "url": "http://www.epicurious.com/recipes/food/views/Chicken-Breast-with-Salsa-242444"
-          }
-        },
-        {
-          "recipe": {
-            "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_a95b1340fb3d2945287a4b31daf1c94f",
-            "label": "Grilled stuffed chicken breast",
-            "image": "https://www.edamam.com/web-img/ac9/ac9d4c23885c77b2747a798ff756b862.jpg",
-            "source": "La Cucina Italiana",
-            "url": "http://lacucinaitalianamagazine.com/recipe/grilled_stuffed_chicken_breast_"
-          }
-        },
-        {
-          "recipe": {
-            "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_7b401f7932461d2c200de073b27f293c",
-            "label": "Chicken Breast with Fresh Sage",
-            "image": "https://www.edamam.com/web-img/80b/80b93a7122b7146dea2af3212a4f64a9.JPG",
-            "source": "Food52",
-            "url": "https://food52.com/recipes/2451-chicken-breast-with-fresh-sage"
-          }
-        },
-        {
-          "recipe": {
-            "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_121b40b4af5f8f9c3dbc0272cc1a70a5",
-            "label": url,
-            "image": "https://www.edamam.com/web-img/b4e/b4ecf425e5fcd21390a1203976f5fef8.jpg",
-            "source": "Epicurious",
-            "url": "http://www.epicurious.com/recipes/food/views/Chicken-Breast-with-Salsa-242444"
-          }
-        }
-      ]
-    }
-
-    hits = response[:hits]
+    response = HTTParty.get(url)
+    hits = response.parsed_response["hits"]
     recipes = []
 
     if hits.count > 0
       hits.each do |hit|
-        url = hit[:recipe][:url]
-        title = hit[:recipe][:label]
-        image = hit[:recipe][:image]
+        url = hit["recipe"]["url"]
+        title = hit["recipe"]["label"]
+        image = hit["recipe"]["image"]
         recipes.push({ url: url, title: title, image: image })
       end
     end
+
+    # Test data to avoid making repeated API calls
+    # response = {
+    #   "hits": [
+    #     {
+    #       "recipe": {
+    #         "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_121b40b4af5f8f9c3dbc0272cc1a70a5",
+    #         "label": params[:query],
+    #         "image": "https://www.edamam.com/web-img/b4e/b4ecf425e5fcd21390a1203976f5fef8.jpg",
+    #         "source": "Epicurious",
+    #         "url": "http://www.epicurious.com/recipes/food/views/Chicken-Breast-with-Salsa-242444"
+    #       }
+    #     },
+    #     {
+    #       "recipe": {
+    #         "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_a95b1340fb3d2945287a4b31daf1c94f",
+    #         "label": "Grilled stuffed chicken breast",
+    #         "image": "https://www.edamam.com/web-img/ac9/ac9d4c23885c77b2747a798ff756b862.jpg",
+    #         "source": "La Cucina Italiana",
+    #         "url": "http://lacucinaitalianamagazine.com/recipe/grilled_stuffed_chicken_breast_"
+    #       }
+    #     },
+    #     {
+    #       "recipe": {
+    #         "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_7b401f7932461d2c200de073b27f293c",
+    #         "label": "Chicken Breast with Fresh Sage",
+    #         "image": "https://www.edamam.com/web-img/80b/80b93a7122b7146dea2af3212a4f64a9.JPG",
+    #         "source": "Food52",
+    #         "url": "https://food52.com/recipes/2451-chicken-breast-with-fresh-sage"
+    #       }
+    #     },
+    #     {
+    #       "recipe": {
+    #         "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_121b40b4af5f8f9c3dbc0272cc1a70a5",
+    #         "label": url,
+    #         "image": "https://www.edamam.com/web-img/b4e/b4ecf425e5fcd21390a1203976f5fef8.jpg",
+    #         "source": "Epicurious",
+    #         "url": "http://www.epicurious.com/recipes/food/views/Chicken-Breast-with-Salsa-242444"
+    #       }
+    #     }
+    #   ]
+    # }
+    #
+    # hits = response[:hits]
+    # recipes = []
+    #
+    # if hits.count > 0
+    #   hits.each do |hit|
+    #     url = hit[:recipe][:url]
+    #     title = hit[:recipe][:label]
+    #     image = hit[:recipe][:image]
+    #     recipes.push({ url: url, title: title, image: image })
+    #   end
+    # end
 
     render json: { recipes: recipes }
   end
