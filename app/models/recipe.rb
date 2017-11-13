fad47e11b0ba91468a5b7a55539eb581f0b85040
jtbirney <jtbirney@gmail.com>
2017-11-13 17:23:43 -0500

    Refactoring of get_ingredients and get_directions methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -46,10 +46,11 @@ class Recipe < ApplicationRecord
       ingredients = ["Could not get ingredients from the url"]
     else
       doc.traverse do |node|
-        if node.children.length == 0 && node.parent.name != 'script'
-          if node.text =~ /^\s*\d.{0,6}(cup|c.|teaspoon|tablespoon|tbsp|tsp|egg|pounds|ounces)/i
-            ingredients = get_parent_text(node)
+        if node.children.length == 0 && node.parent.name != 'script' && node.parent.name != 'meta' && node.parent.name != 'style'
+          if node.text =~ /^\s*\d.{0,6}(cup|c\.|teaspoon|tablespoon|tbsp|tsp|egg|pounds|ounces)/i
             parent_node = get_parent_node(node)
+            ingredients = children_text(parent_node)
+            ingredients = remove_whitespace(ingredients)
             ingredients.concat(check_next_node_ingredients(parent_node))
             break
           end
@@ -58,25 +59,6 @@ class Recipe < ApplicationRecord
     end
     ingredients = ingredients.uniq
     ingredients
-  end
-
-  def check_next_node_ingredients(node)
-    text_array = []
-    sibling_nodes = node.parent.children
-    index = node.parent.children.index(node)
-    sliced_nodes = sibling_nodes[index + 1..sibling_nodes.length]
-    sliced_nodes.each do |node|
-      if node.text =~ /^\s*\d.{0,6}(cup|c.|teaspoon|tablespoon|tbsp|tsp|egg|pounds|ounces)/i
-        text = node.text
-        text.strip!
-        if text.downcase != "advertisement"
-          text_array << node.text
-        end
-      end
-    end
-
-    text_array = remove_whitespace(text_array)
-    text_array
   end
 
   def get_directions
@@ -92,9 +74,10 @@ class Recipe < ApplicationRecord
     else
       doc.traverse do |node|
         if node.children.length == 0 && node.parent.name != 'script' && node.parent.name != 'style'
-          if node.text =~ /((cook|bake|roast|simmer|grill).{1,40}\d minute|heat.*\d\d\d°F|(place|combine|mix|whisk|cover).*(bowl|pot|pan))|((bowl|pot|pan).*(place|combine|mix|whisk|cover))/i
-            directions = get_parent_text(node)
+          if node.text =~ /((cook|bake|roast|simmer|grill).{1,100}\d minute|heat.*\d\d\d°F|(place|combine|mix|whisk|cover).*(bowl|pot|pan))|((bowl|pot|pan).*(place|combine|mix|whisk|cover))/i
             parent_node = get_parent_node(node)
+            directions = children_text(parent_node)
+            directions = remove_whitespace(directions)
             directions.concat(check_next_node_directions(parent_node))
             break
           end
@@ -103,9 +86,6 @@ class Recipe < ApplicationRecord
     end
     directions = directions.uniq
     if ingredients
-      # ingredients.each do |ingredient|
-      #   directions.delete(ingredient)
-      # end
       last_ingredient = ingredients.last
       index = directions.index(last_ingredient)
       if index
@@ -116,34 +96,7 @@ class Recipe < ApplicationRecord
     directions
   end
 
-  def check_next_node_directions(node)
-    text_array = []
-    parent_node = get_parent_node(node)
-    sibling_nodes = parent_node.children
-    # index = parent_node.children.index(node)
-    # sliced_nodes = sibling_nodes[index + 1..sibling_nodes.length]
-    sibling_nodes.each do |node|
-      if node.text =~ /((cook|bake|roast|simmer|grill).{1,40}\d minute|heat.*\d\d\d°F|(place|combine|mix|whisk|cover).*(bowl|pot|pan))|((bowl|pot|pan).*(place|combine|mix|whisk|cover))/i
-        text = node.text
-        text.strip!
-        if text =~ /^\d*\./
-          text_less_number_array = text.split(/^\d*\./)
-          new_text = ""
-          text_less_number_array.each do |string|
-            string.strip!
-            new_text += string
-          end
-          text = new_text
-        end
-        numbered_list_regexp = /^\d*(\.|)$/
-        if !(text =~ numbered_list_regexp) && text.downcase != "advertisement"
-          text_array << node.text
-        end
-      end
-    end
-    text_array = remove_whitespace(text_array)
-    text_array
-  end
+  private
 
   def get_parent_node(node)
     parent_node = nil
@@ -165,7 +118,7 @@ class Recipe < ApplicationRecord
           end
           text = new_text
         end
-        if text.downcase != "advertisement"
+        if text.downcase != "advertisement" && !(text =~ /react-text/i)
           parent_array << node.text
         end
       end
@@ -181,12 +134,48 @@ class Recipe < ApplicationRecord
     parent_node
   end
 
-  def get_parent_text(node)
-    node_array = []
-    node_array << node.text
-    parent_array = []
-    node.parent.children.each do |node|
-      if node.name != ("strong" || "em" || "a")
+  def children_text(node)
+    text_array = []
+    text_string = ""
+    node.children.each do |node|
+      if node.children.count == 0
+        text_string += node.text
+        text_array << text_string
+        text_string = ""
+      elsif node.name == ("strong" || "em" || "a")
+        text_string = node.text
+      else
+        text_array.concat(children_text(node))
+      end
+    end
+    text_array
+  end
+
+  def check_next_node_ingredients(node)
+    text_array = []
+    sibling_nodes = node.parent.children
+    index = node.parent.children.index(node)
+    sliced_nodes = sibling_nodes[index + 1..sibling_nodes.length]
+    sliced_nodes.each do |node|
+      if node.text =~ /^\s*\d.{0,6}(cup|c.|teaspoon|tablespoon|tbsp|tsp|egg|pounds|ounces)/i
+        text = node.text
+        text.strip!
+        if text.downcase != "advertisement" && !(text =~ /react-text/i)
+          text_array << node.text
+        end
+      end
+    end
+
+    text_array = remove_whitespace(text_array)
+    text_array
+  end
+
+  def check_next_node_directions(node)
+    text_array = []
+    parent_node = get_parent_node(node)
+    sibling_nodes = parent_node.children
+    sibling_nodes.each do |node|
+      if node.text =~ /((cook|bake|roast|simmer|grill).{1,100}\d minute|heat.*\d\d\d°F|(place|combine|mix|whisk|cover).*(bowl|pot|pan))|((bowl|pot|pan).*(place|combine|mix|whisk|cover))/i
         text = node.text
         text.strip!
         if text =~ /^\d*\./
@@ -198,36 +187,15 @@ class Recipe < ApplicationRecord
           end
           text = new_text
         end
-        if text.downcase != "advertisement"
-          parent_array << node.text
+        numbered_list_regexp = /^\d*(\.|)$/
+        if !(text =~ numbered_list_regexp) && text.downcase != "advertisement" && !(text =~ /react-text/i)
+          text_array.concat(children_text(node))
         end
       end
     end
-
-    node_array = remove_whitespace(node_array)
-    parent_array = remove_whitespace(parent_array)
-    if parent_array == node_array
-      parent_array = get_parent_text(node.parent)
-    end
-    parent_array
+    text_array = remove_whitespace(text_array)
+    text_array
   end
-
-  # def get_child_text(node)
-  #   text_array = []
-  #   if node.children.length == 0
-  #     text = node.text
-  #     text.strip!
-  #     numbered_list_regexp = /^\d*(.|)$/
-  #     if !(text =~ numbered_list_regexp) && text.downcase != "advertisement"
-  #       text_array << text
-  #     end
-  #   else
-  #     node.children.each do |node|
-  #       text_array = get_child_text(node)
-  #     end
-  #   end
-  #   text_array
-  # end
 
   def remove_whitespace(array)
     stripped_array = []
@@ -237,7 +205,7 @@ class Recipe < ApplicationRecord
       string_array.each do |string|
         string.strip!
         numbered_list_regexp = /^\d*(\.|)$/
-        if !(string =~ numbered_list_regexp) && string != "" && string.downcase != "ingredients" && string.downcase != "advertisement"
+        if !(string =~ numbered_list_regexp) && string != "" && string.downcase != "ingredients" && string.downcase != "advertisement" && !(string =~ /react-text/i)
           stripped_array << string
         end
       end
